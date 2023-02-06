566ed54ae4f95083aa19cdd1e652ae6dcb53960c
annaleethompson <acthompson@smith.edu>
2023-02-05 22:42:31 -0500

    Day1

--- computer.py
+++ computer.py
@@ -1,10 +1,35 @@
 class Computer:
 
     # What attributes will it need?
+    description: str
+    processor_type: str
+    hard_drive_capacity: int
+    memory: int
+    operating_system: str
+    year_made: int
+    price: int
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
+    def __init__(item, description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):
+        item.descrip = description
+        item.proctype = processor_type
+        item.harddrive = hard_drive_capacity
+        item.memory = memory
+        item.operatingsyst = operating_system
+        item.year = year_made
+        item.price = price
+        
     # What methods will you need?
+        #storing information about a specific computer
+        #updating a computer's OS
+
+    def create_computer(description, processor_type, hard_drive_capacity, memory, operating_system, year_made, price):
+        return {'description': description,
+            'processor_type': processor_type,
+            'hard_drive_capacity': hard_drive_capacity,
+            'memory': memory,
+            'operating_system': operating_system,
+            'year_made': year_made,
+            'price': price
+    }
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,41 @@
+from computer import *
+
 class ResaleShop:
 
     # What attributes will it need?
-
+    inventory = []
+    itemID = 0
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__():
         pass # You'll remove this when you fill out your constructor
 
     # What methods will you need?
+    def buy(self, inventory):
+        #Call computer constructor
+        computer = computer.create_computer("Mac Pro (Late 2013)", "3.5 GHc 6-Core Intel Xeon E5", 1024, 64,"macOS Big Sur", 2013, 1500)
+
+        # Add it to the resale store's inventory
+        print("Buying", computer["description"])
+        print("Adding to inventory...")
+    
+        #Call inventory.append() to add new computer
+        inventory.append(computer)
+        itemID += 1 # increment itemID
+        inventory[itemID] = computer
+        computer_id = itemID
+        print("Done.\n")
+    
+    def sell(self):
+        pass
+        
+
+def main():
+     print("-" * 21)
+     print("COMPUTER RESALE STORE")
+     print("-" * 21)
+
+     # Add it to the resale store's inventory
+     
+
+        
